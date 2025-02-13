1d26101dade4af8008c99517f7befaa99d7ad1ec
AgilaThiruchelvam18 <agilaagi18@gmail.com>
2025-02-13 14:26:24 +0530

    final

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -7,7 +7,7 @@ function Dashboard(){
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <div className="bg-white p-6 rounded-lg shadow-lg w-96">
        <div className="flex flex-row justify-between"><h2 className="text-2xl font-bold text-center mb-4">Dashboard</h2>
-          <Link to={"/Login"}>Back</Link></div>   
+          <Link to="/login">Back</Link></div>   
           <p className="text-green-500 text-center">Welcome to the dashboard</p>
         </div>
       </div>
